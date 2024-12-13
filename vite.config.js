46ef671cbdd535d005b309acc9e1c99de328d5b2
ReshmiRahul <reshmihl94@gmail.com>
2024-12-12 23:37:04 -0500

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'MyPortfolio', // Adjust if deploying to subdirectories, e.g., '/your-repo-name/' for GitHub Pages
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specifies the output directory for production builds
+  },
+});
