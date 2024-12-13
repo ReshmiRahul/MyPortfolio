17e38d0e92f596a0f5b3a67154c89355c380e3ee
ReshmiRahul <reshmihl94@gmail.com>
2024-12-13 00:30:19 -0500

    made changes in the portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'MyPortfolio', // Adjust if deploying to subdirectories, e.g., '/your-repo-name/' for GitHub Pages
+  base: '/MyPortfolio/', // Ensure this is the correct path
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Specifies the output directory for production builds
-  },
 });
